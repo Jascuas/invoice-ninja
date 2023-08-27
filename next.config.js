b47a524b0ffcaa017cb825603d05cafd6bcbe050
JASCUAS\javie <javiersanchez@erescue.com>
2023-08-27 13:42:01 +0200

    pdf reader

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+
+
+const nextConfig = {
+    experimental: {
+        serverComponentsExternalPackages: [],
+    },
+};
+
 
 module.exports = nextConfig
