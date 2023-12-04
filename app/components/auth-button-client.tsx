739128aecd6d67dcebecb24cfba8c44f6c4c0fc1
JASCUAS\javie <javiersanchez@erescue.com>
2023-12-04 12:50:45 +0100

    callback url

--- app/components/auth-button-client.tsx
+++ app/components/auth-button-client.tsx
@@ -20,7 +20,7 @@ export function AuthButton({ session }: { session: Session | null }) {
     const res = await supabase.auth.signInWithOAuth({
       provider: "github",
       options: {
-        redirectTo: window?.location?.origin + "/auth/callback",
+        redirectTo: process.env.NEXT_PUBLIC_VERCEL_URL + "/auth/callback",
       },
     });
     console.log(res);
